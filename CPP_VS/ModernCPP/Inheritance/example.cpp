e49a5b14462ac6bd4875b1542b66d4161d29e755
“ShutterSpeed <shutterspeedlabs@gmail.com>
2024-05-09 11:12:59 +0530

    init

---
+++ CPP_VS/ModernCPP/Inheritance/example.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * The main function is the entry point of every C++ program. It is where
+ * the program starts executing. The function is called by the C++ runtime
+ * environment without any parameters.
+ *
+ * The function simply prints "Hello World" to the console using the C++
+ * standard output stream object cout and the endl manipulator to insert a
+ * newline character at the end of the string.
+ *
+ * The function returns an integer as the program's exit code. In this case
+ * the exit code is 0 which indicates that the program executed successfully.
+ */
+int main(){
+    // Print "Hello World" to the console
+    cout << "Hello World" << endl; // The endl inserts a newline character
+
+    // Return 0 as the program's exit code
+    return 0;
+}
